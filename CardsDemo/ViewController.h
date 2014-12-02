32fe214423ae6e142dcda74cb3788bdb7d737f18
Sachin Medhekar <sachinmedhekar@Sachins-MacBook-Pro.local>
2014-12-01 23:58:00 -0800

    Created Repo from Singe View Application

---
+++ CardsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardsDemo
+//
+//  Created by Sachin Medhekar on 12/1/14.
+//  Copyright (c) 2014 Sachin Medhekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardsDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CardsDemo
+//
+//  Created by Sachin Medhekar on 12/1/14.
+//  Copyright (c) 2014 Sachin Medhekar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
